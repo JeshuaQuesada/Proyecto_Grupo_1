991478a4bc27d4bc0e391da1c3d9a61050f8e8d6
Jesh20 <qjeshua50952@ufide.ac.cr>
2025-04-03 00:07:59 -0600

    html

---
+++ Libreria_Libreria_jar_0.0.1-SNAPSHOT/src/main/java/Portafolio/Portafolio/PortafolioJeshuaApplication.java
@@ -0,0 +1,13 @@
+package Portafolio.Portafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioJeshuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioJeshuaApplication.class, args);
+	}
+
+}
