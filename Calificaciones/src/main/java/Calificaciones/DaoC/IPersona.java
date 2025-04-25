9e58aaaafe98150a91b627150ce63ff744e06ea9
luckschavalo <lucaschl1007@gmail.com>
2025-04-24 22:16:00 -0600

    Delete Calificaciones directory

--- Calificaciones/src/main/java/Calificaciones/DaoC/IPersona.java
+++
@@ -1,9 +0,0 @@
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import Calificaciones.DomainC.Persona;
-
-package Calificaciones.DaoC;
-
-public class IPersona extends CrudRepository<Persona,Integer>{
-}
